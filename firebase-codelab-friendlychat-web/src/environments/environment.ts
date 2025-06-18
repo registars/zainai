b97469a2b5e85b7407b01ff51380b6d065dff276
magma <maghfuri3m@gmail.com>
2025-06-18 09:18:52 +0700

    Update environment.ts

--- firebase-codelab-friendlychat-web/src/environments/environment.ts
+++ firebase-codelab-friendlychat-web/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
+    apiKey: 'AIzaSyDIDeCNx9FO1F64L3ZHOm-o5w6kqmcbdKU',
+    authDomain: 'zainaichat.firebaseapp.com',
     databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
+    projectId: 'zainaichat',
+    storageBucket: 'zainaichat.firebasestorage.app',
+    messagingSenderId: '486846932304',
+    appId: '1:486846932304:web:bed0a39e3aa5dbb85df855',
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
